51faa65e1547246b035b5077f4cc4fd00163aa56
tanay <70639071+tnb24@users.noreply.github.com>
2021-09-07 11:20:24 +0530

    indent

--- scripts/vsc.zsh
+++ scripts/vsc.zsh
@@ -2,5 +2,5 @@
 
 cat vscode/extensions.ubuntu | while read extension || [[ -n $extension ]]; do
     code --install-extension $extension --force
-  done
+done
 # took from https://github.com/daniellwdb/dotfiles/blob/main/install.zsh
